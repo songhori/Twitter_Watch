011977155c420f1556ecfcc7e40d25eaca395a19
Mohammad Mahdi Songhori <112444581+songhori@users.noreply.github.com>
2023-03-15 04:53:03 +0000

    removed unused packages and fixed active users.

--- sentiment.py
+++ sentiment.py
@@ -3,23 +3,23 @@ import re
 
 
 def clear_text(text):
-  # Use re.sub() to replace all mentions with an empty string
-  cleaned_text = re.sub(r'@[A-Za-z0-9]+', '@', text)
-  cleaned_text = re.sub(r'http\S+|www\S+', 'http', cleaned_text)
-  return cleaned_text
+    # Use re.sub() to replace all mentions with an empty string
+    cleaned_text = re.sub(r'@[A-Za-z0-9]+', '@', text)
+    cleaned_text = re.sub(r'http\S+|www\S+', 'http', cleaned_text)
+    return cleaned_text
 
 def check_polarity(num):
     if num <= -.5:
-        return 'Negative'
+       return 'Negative'
     elif num >= .5:
-        return 'Positive'
+       return 'Positive'
     else:
-        return 'Neutral'
+       return 'Neutral'
 
 def sentiment_analyzer(text):
-  cleaned_text = clear_text(text)
-  analyzer = SentimentIntensityAnalyzer()
-  metric = analyzer.polarity_scores(cleaned_text)['compound']
-  polarity = check_polarity(metric)
-  result = {'polarity': polarity, 'metric':metric}
-  return result
+    cleaned_text = clear_text(text)
+    analyzer = SentimentIntensityAnalyzer()
+    metric = analyzer.polarity_scores(cleaned_text)['compound']
+    polarity = check_polarity(metric)
+    result = {'polarity': polarity, 'metric':metric}
+    return result
--- summarise.py
+++ summarise.py
@@ -13,24 +13,23 @@ from heapq import nlargest
 
 
 def summarise(text):
-  # tokenize the sentences and words in text
-  sentences = sent_tokenize(text)
-  words = word_tokenize(text)
-
-  # remove stopwords and stem remaining words
-  stop_words = set(stopwords.words('english'))
-  filtered_words = [
-    PorterStemmer().stem(w.lower()) for w in words if not w in stop_words]
-
-  # calculate word frequency
-  freq_dist = FreqDist(filtered_words)
-  top_words = nlargest(2, freq_dist, key=freq_dist.get)
-
-  # summarize the text with top sentences that contain the most important words
-  summary = []
-  for sentence in sentences:
-    if any(word in sentence.lower() for word in top_words):
-      summary.append(sentence)
-
-  summary = '\n'.join(summary)
-  return summary
+    # tokenize the sentences and words in text
+    sentences = sent_tokenize(text)
+    words = word_tokenize(text)
+  
+    # remove stopwords and stem remaining words
+    stop_words = set(stopwords.words('english'))
+    filtered_words = [PorterStemmer().stem(w.lower()) for w in words if not w in stop_words]
+  
+    # calculate word frequency
+    freq_dist = FreqDist(filtered_words)
+    top_words = nlargest(2, freq_dist, key=freq_dist.get)
+  
+    # summarize the text with top sentences that contain the most important words
+    summary = []
+    for sentence in sentences:
+        if any(word in sentence.lower() for word in top_words):
+           summary.append(sentence)
+  
+    summary = '\n'.join(summary)
+    return summary
